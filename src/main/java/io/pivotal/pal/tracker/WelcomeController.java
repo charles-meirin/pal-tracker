606ab24395560b2521b8e761ba7fc2cc0c4fc65c
Ubuntu <ubuntu@ip-172-31-35-122.ec2.internal>
2021-06-15 20:14:44 +0000

    Deployment Pipelines LAB 2

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -1,2 +1,20 @@
-package io.pivotal.pal.tracker;public class WelcomeController {
+package io.pivotal.pal.tracker;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+    String message;
+
+    @GetMapping("/")
+    public String sayHello() {
+        return message;
+    }
+
+    public WelcomeController(@Value("${welcome.message}") String message){
+        this.message = message;
+    }
 }
